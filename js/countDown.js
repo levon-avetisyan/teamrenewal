551ab489fc5da3fa33828e61ba07c67185198b31
levon-avetisyan <levon.s.avetisyan@gmail.com>
2018-11-05 15:41:11 +0400

    build

--- js/countDown.js
+++ js/countDown.js
@@ -30,7 +30,7 @@ function countdown(endDate) {
             document.getElementById("days").innerHTML = parseInt(days, 10);
             document.getElementById("hours").innerHTML = ("0" + hours).slice(-2);
             document.getElementById("minutes").innerHTML = ("0" + minutes).slice(-2);
-            document.getElementById("seconds").innerHTML = ("0" + seconds).slice(-2);
+            // document.getElementById("seconds").innerHTML = ("0" + seconds).slice(-2);
         } else {
             return;
         }
