941fe4a679136ed71638593ecec2d4191ec334c4
levon-avetisyan <levon.s.avetisyan@gmail.com>
2018-11-05 23:14:23 +0400

    dontaform

--- js/all.js
+++ js/all.js
@@ -6,6 +6,25 @@ $(document).ready(function () {
         $('html,body').animate({ scrollTop: $("#leaderboard").offset().top }, 'slow');
     });
     $('.map-image-wrap').zoom({ url: '../img/map-big.jpg' });
+
+    window.onscroll = function () {
+        myFunction();
+    };
+
+    // Get the header
+    var header = document.getElementById("siteNavTop");
+
+    // Get the offset position of the navbar
+    var sticky = header.offsetTop;
+
+    // Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+    function myFunction() {
+        if (window.pageYOffset > sticky) {
+            header.classList.add("sticky");
+        } else {
+            header.classList.remove("sticky");
+        }
+    }
 });
 
 },{}]},{},[1]);
