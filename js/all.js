59d7c91b549d0d60dd03d4418199d8f0fd385b8b
levon-avetisyan <levon.s.avetisyan@gmail.com>
2018-11-07 16:31:08 +0400

    build6

--- js/all.js
+++ js/all.js
@@ -5,7 +5,46 @@ $(document).ready(function () {
     $('#sponsorAction').click(function () {
         $('html,body').animate({ scrollTop: $("#leaderboard").offset().top }, 'slow');
     });
+    //Nav click slick smooth scroll
+    // Select all links with hashes
+    $('a[href*="#"]')
+    // Remove links that don't actually link to anything
+    .not('[href="#"]').not('[href="#0"]').click(function (event) {
+        // On-page links
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            // Figure out element to scroll to
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            // Does a scroll target exist?
+            if (target.length) {
+                // Only prevent default if animation is actually gonna happen
+                event.preventDefault();
+                $('html, body').animate({
+                    scrollTop: target.offset().top
+                }, 1000, function () {
+                    // Callback after animation
+                    // Must change focus!
+                    var $target = $(target);
+                    $target.focus();
+                    if ($target.is(":focus")) {
+                        // Checking if the target was focused
+                        return false;
+                    } else {
+                        $target.attr('tabindex', '-1'); // Adding tabindex for elements not focusable
+                        $target.focus(); // Set focus again
+                    };
+                });
+            }
+        }
+    });
+
+    // Map image zoom
     $('.map-image-wrap').zoom({ url: './img/map-big.jpg' });
+
+    // Close nav on link click
+    $('.navbar-nav>li>a').on('click', function () {
+        $('.navbar-collapse').collapse('hide');
+    });
 
     // Sticky Nav
     window.onscroll = function () {
@@ -51,7 +90,6 @@ $(document).ready(function () {
     $("#profilePhotoUpload").change(function () {
         readURL(this);
     });
-
 });
 
 },{}]},{},[1]);
